ee220cb431f62e8f042e4ed9e13a9ebc2fcaacf2
Matt Ralph <mralph@equalexperts.com>
2019-06-12 13:02:14 +0100

    Working ridley-spotter for HTML

---
+++ defaults.py
@@ -0,0 +1,1 @@
+target_url = "https://www.churchofengland.org/more/diocesan-resources/ministry/training-institutions"
---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import requests
+from lxml import html
+from defaults import target_url
+import hashlib
+from bs4 import BeautifulSoup
+
+
+def tell_matt(ridley_present):
+    if ridley_present:
+        print("Ridley report found!")  #  TODO email
+    else:
+        print("No Ridley report found")
+
+
+def main():
+    response = requests.get(target_url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    block_content = soup.find("div", {"id": "block-cofe-content"})
+    article = block_content.article
+    reports_div = article.find_all("div")[16].div.div
+    reports_2019 = reports_div.ul.find_all("li")
+    for report in reports_2019:
+        title = report.text
+        institution_name = title.split("-")[0].rstrip()
+        print(institution_name)
+        if "Ridley" in institution_name:
+            tell_matt(True)
+            return True
+
+    tell_matt(False)
+
+
+if __name__ == "__main__":
+    main()
